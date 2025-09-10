cc763775f6258f4d6692d41eb3bba0e03858a50c
NadiaHirwa <irahirnad22@gmail.com>
2025-09-10 22:16:02 +0300

    CRUD update

---
+++ app/api/tasks/[id]/route.js
@@ -0,0 +1,19 @@
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "../auth/[...nextauth]/route"
+import { prisma } from "@/lib/prisma" // we'll create this prisma client file
+
+// 🔹 Update task (PUT) & Delete task (DELETE)
+export async function PUT(req, { params}) {
+    const session = await getServerSession(authOptions)
+    if (!session) return new Response("Unauthorized", { status: 401 })
+
+    const body = await req.json()
+    const taskId = parseInt(params.id) 
+
+    const updated = await prisma.task.update({
+      where: { id: taskId, userId: session.user.id },
+      data: { title: body.title, completed: body.completed },
+    })
+
+    return Response.json(updated)
+}
